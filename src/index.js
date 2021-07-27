085bc8b1e5e1a9b54bca9f6afb8d378c3a0d800c
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-07-26 23:01:20 -0400

    added some generic responses to correct or incorrect responses

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+import disasters from "./scripts/disasters"
+
 document.addEventListener("DOMContentLoaded", () => {
+    const disaster = shuffle(disasters)[0]
+    let solutionCards = document.querySelectorAll('.card')
+    let disasterCard = document.querySelector('.disaster-card')
+    disasterCard.innerHTML= disaster.disaster.description
+    let j = 0
+    let cardIndexes= shuffle([0, 1, 2]);
+    for(var i = 0; i < solutionCards.length; i++) {
+        var card = solutionCards[i];
+        card.querySelector('.card-text').innerHTML = disaster.solutions[cardIndexes[j]].description
+        card.querySelector('.card-answer').innerHTML = disaster.solutions[cardIndexes[j]].answer
+        j++
+        card.addEventListener("click", function(e){
+            answer(this)
+        });
+    }
+
+    function answer(card){
+        // card.classList.add("answer-card")
+        // console.log(card.querySelector('.card-answer').innerHTML === 'true')
+        if(card.querySelector('.card-answer').innerHTML === 'true') alert("Correct")
+        else alert("Incorrect")
+    }
+
+    function shuffle(array) {
+        var currentIndex = array.length,  randomIndex;
+    
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+        
+            [array[currentIndex], array[randomIndex]] = [
+                array[randomIndex], array[currentIndex]];
+        }
+    
+        return array;
+    }
     
 })
---
+++ src/scripts/disasters.js
@@ -0,0 +1,62 @@
+let disasters = [
+    {
+        disaster: {
+            description : "A Violent Volcano Erupts on your island",
+        },
+        solutions: [
+            {
+                description : "Get as close as you can for a selfie.",
+                answer : false,
+            },
+            {
+                description : "Get on a boat to escape",
+                answer : true,
+            },
+            {
+                description : "Swim out of there",
+                answer : false,
+            }
+        ]
+    },
+    {
+        disaster: {
+            description : "Letters",
+        },
+        solutions: [
+            {
+                description : "Get as close as you can for a selfie.",
+                answer : false,
+            },
+            {
+                description : "Get on a boat to escape",
+                answer : true,
+            },
+            {
+                description : "Swim out of there",
+                answer : false,
+            }
+        ]
+    },
+
+    {
+        disaster: {
+            description : "An Ocean Crashes into the mall",
+        },
+        solutions: [
+            {
+                description : "A.",
+                answer : false,
+            },
+            {
+                description : "B",
+                answer : true,
+            },
+            {
+                description : "C",
+                answer : false,
+            }
+        ]
+    }
+];
+
+export default disasters
